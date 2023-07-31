f6399b5c9bbb09d54b00a2548cf485f54adb4806
iconshah <shahicon691@gmail.com>
2023-07-31 08:40:56 +0545

    minor changes

--- mail.php
+++ mail.php
@@ -3,7 +3,7 @@
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         # FIX: Replace this email with recipient email
-         $mail_to = "Put your email here";
+         $mail_to = "shahicon691@gmail.com";
          
         # Sender Data
         $subject = trim($_POST["subject"]);
